d46f0e726bcaf29da89bc27a74f62cd09c993f24
Testing_author <testing_beta@mail.com>
2020-12-28 15:37:58 +0200

    Writing a test which will verify the sums of a sequence of numbers.

--- Sources/MyLibrary/Sequence.swift
+++ Sources/MyLibrary/Sequence.swift
@@ -11,3 +11,17 @@ public extension Sequence {
         first { _ in true }
     }
 }
+
+
+public extension Sequence where Element: AdditiveArithmetic {
+    
+    var sum: Element? {
+        
+        guard let first = first else {
+            return nil
+        }
+        return dropFirst().reduce(first, +)
+    }
+}
+
+
--- Tests/MyLibraryTests/SequenceTestCase.swift
+++ Tests/MyLibraryTests/SequenceTestCase.swift
@@ -17,4 +17,24 @@ final class SequenceTestCase: XCTestCase {
         XCTAssertEqual(1, odds.first)
         XCTAssertNil(odds.prefix(0).first)
     }
+    
+    
+    func test_sum() {
+        let threetwoOne = stride(from: 3, through: 1, by: -1)
+        XCTAssertEqual(threetwoOne.sum, 6)
+    
+        XCTAssertEqual([0.5, 1, 1.5].sum, 3)
+        XCTAssertNil(Set<CGFloat>().sum)
+    
+        
+        let oneThird = 1.0 / 3
+        let theirSum = Array(repeating: oneThird, count: 30).sum
+        XCTAssertEqual(theirSum, 10)
+        
+        let oneThirdish = 1.0 / 3
+        let thirdishSum = Array(repeating: oneThirdish, count: 300).sum
+        XCTAssertEqual(
+            try XCTUnwrap(thirdishSum), 100, accuracy: pow(0.1, 12))
+        
+    }
 }
